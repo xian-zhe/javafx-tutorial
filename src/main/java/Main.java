d9c6e751b5d1a273e4aa76f21b4275234c3cb356
xian zhe <e1511868@u.nus.edu>
2026-09-04 16:20:09 +0800

    Add dialog box for Duke's response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Duke duke = new Duke();
 
     // Existing constructor
     public Main(String filePath) {
@@ -96,7 +97,13 @@ public class Main extends Application {
      * the dialog container. Clears the user input after processing.
      */
     private void handleUserInput() {
-        dialogContainer.getChildren().addAll(new DialogBox(userInput.getText(), userImage));
+        String userText = userInput.getText();
+        String dukeText = duke.getResponse(userInput.getText());
+        dialogContainer.getChildren().addAll(
+                new DialogBox(userText, userImage),
+                new DialogBox(dukeText, dukeImage)
+        );
         userInput.clear();
     }
+
 }
